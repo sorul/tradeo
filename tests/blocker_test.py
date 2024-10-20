53ef410fa6e276d37cf7265c83558f50944dfd1a
sorul <cromerovargas2d@gmail.com>
2024-10-20 16:30:21 +0200

    class and test finished

---
+++ tests/blocker_test.py
@@ -0,0 +1,82 @@
+import os
+import time
+import pytest
+from unittest import mock
+from tradeo.context_managers.blocker import Blocker
+
+
+@pytest.fixture
+def _cleanup_lockfile():
+  lockfile = '/tmp/tradeo_test.lock'
+  yield
+  if os.path.exists(lockfile):
+    os.remove(lockfile)
+
+
+def test_blocker_creates_lockfile(_cleanup_lockfile):
+  """Test that the lock file is created."""
+  with Blocker('/tmp/tradeo_test.lock'):
+    assert os.path.exists(
+        '/tmp/tradeo_test.lock'), 'The lock file was not created.'
+
+
+@mock.patch('psutil.pid_exists', return_value=True)
+def test_blocker_prevents_execution_if_lock_exists(mock, _cleanup_lockfile):
+  """Test that execution is prevented if a lock file already exists.
+
+  And the process is still running.
+  """
+  # We create a lock file manually
+  with open('/tmp/tradeo_test.lock', 'w') as f:
+    f.write('1234,{}'.format(time.time()))
+
+  # Simulating process still alive with mock
+  with pytest.raises(SystemExit):
+    with Blocker('/tmp/tradeo_test.lock'):
+      pass
+
+
+def test_blocker_removes_lockfile_on_exit(_cleanup_lockfile):
+  """Test that the lock file is removed upon exiting the context."""
+  with Blocker('/tmp/tradeo_test.lock'):
+    pass
+  assert not os.path.exists(
+      '/tmp/tradeo_test.lock'), 'The lock file was not removed.'
+
+
+@mock.patch('psutil.pid_exists', return_value=False)
+def test_blocker_removes_stale_lockfile(mock, _cleanup_lockfile):
+  """Test - Stale lock file is removed if the process no longer exists."""
+  # We create a lock file with a non-existent PID and old timestamp
+  with open('/tmp/tradeo_test.lock', 'w') as f:
+    f.write('1234,{}'.format(time.time() - 600))  # 10 minutes ago
+
+  # We run Blocker, it should remove the stale lock file
+  with Blocker('/tmp/tradeo_test.lock', timeout=300):
+    assert os.path.exists(
+        '/tmp/tradeo_test.lock'), 'The lock file should have been recreated.'
+
+
+@mock.patch('psutil.pid_exists', return_value=True)
+def test_blocker_does_not_remove_recent_lockfile(mock, _cleanup_lockfile):
+  """Test that a recent lock file is not removed if the process still exists."""
+  # We create a lock file with an existing PID and recent timestamp
+  with open('/tmp/tradeo_test.lock', 'w') as f:
+    f.write('1234,{}'.format(time.time() - 100))  # Less than 5 minutes ago
+
+  # We run Blocker, it should detect that the file is not stale
+  with pytest.raises(SystemExit):
+    with Blocker('/tmp/tradeo_test.lock', timeout=300):
+      pass
+
+
+def test_blocker_handles_invalid_lockfile_format(_cleanup_lockfile):
+  """Test that a lock file with an invalid format is considered stale."""
+  # We create a lock file with an invalid format
+  with open('/tmp/tradeo_test.lock', 'w') as f:
+    f.write('invalid_content')
+
+  # We run Blocker, it should remove the lock file due to invalid format
+  with Blocker('/tmp/tradeo_test.lock'):
+    assert os.path.exists(
+        '/tmp/tradeo_test.lock'), 'The lock file should have been recreated.'
--- tradeo/context_managers/blocker.py
+++ tradeo/context_managers/blocker.py
@@ -1,29 +1,91 @@
-"""This class blocks the execution writing a .block file."""
-from tradeo.files import write_file, remove_file, file_exists
+"""This class blocks execution by writing a .block file."""
+import os
+import time
+import psutil
+
+from tradeo.log import log
 
 
 class Blocker:
-  """Blocker class."""
+  """
+  A class used to block execution by writing a .block file.
 
-  def __init__(self, name: str) -> None:
-    """Initialize the Blocker."""
-    self.name = f'{name}.block'
+  This class creates a lock file to prevent multiple instances
+  of a script from running simultaneously.
+  It checks if the lock file is stale based on a timeout and the existence
+  of the process whose PID is in the file.
+  """
 
-  def is_blocked(self) -> bool:
-    """Check if the execution is blocked."""
-    return file_exists(self.name)
+  def __init__(self, lockfile='/tmp/tradeo.lock', timeout=300):
+    """
+    Initialize the class with the lock file path and a timeout.
 
-  def __enter__(self) -> 'Blocker':
-    """Blocker enter."""
-    write_file(self.name)
-    return self
+    If the lock file is older than the timeout,it will be automatically removed.
+    it will be automatically removed.
+    :param lockfile: The path to the lock file.
+    :param timeout: Maximum time in seconds that the lock
+    file is considered valid.
+    """
+    self.lockfile = lockfile
+    self.timeout = timeout
 
-  def __exit__(self, exc_type: str, exc_value: str, traceback: str) -> bool:
-    """Blocker exit."""
-    _, _, _ = exc_type, exc_value, traceback
-    remove_file(self.name)
-    return True
+  def is_stale(self):
+    """
+    Check if the lock file is stale.
 
-  def remove_block(self) -> None:
-    """Manually the user can remove the block."""
-    remove_file(self.name)
+    Checks if the lock file is stale
+    or if the process whose PID is in the file no longer exists.
+    """
+    is_stale = True
+    # Check if the lock file exists
+    if not os.path.exists(self.lockfile):
+      return False
+
+    # Read the PID and creation time of the lock file
+    with open(self.lockfile, 'r') as lock_fh:
+      content = lock_fh.read().strip().split(',')
+      if len(content) != 2:
+        # If it doesn't have the expected format, consider it stale
+        is_stale = True
+
+      pid, timestamp = int(content[0]), float(content[1])
+
+      # Check if the process with the PID still exists
+      if is_stale and psutil.pid_exists(pid):
+        # If the process still exists, the file is not stale
+        is_stale = False
+
+      # If the file is too old, consider it stale
+      if not is_stale and time.time() - timestamp > self.timeout:
+        is_stale = True
+
+    return is_stale
+
+  def __enter__(self):
+    """
+    Enters the context and creates the lock file.
+
+    If the file already exists and is not stale, the script exits.
+    If the file is stale, it is removed and a new one is created.
+    """
+    # First, check if the lock file exists
+    if os.path.exists(self.lockfile):
+      # If the file exists, check if it's stale
+      if not self.is_stale():
+        # If it's not stale, exit the program
+        log.warning('Another instance is running. Exiting...')
+        exit(0)
+      else:
+        # If the file is stale, remove it
+        log.warning('The lock file is stale. Removing it...')
+        os.remove(self.lockfile)
+
+    # If we get here, it's because the file didn't exist
+    # or was stale and we removed it. Create the new lock file
+    with open(self.lockfile, 'w') as lock_fh:
+      lock_fh.write(f"{os.getpid()},{time.time()}")
+
+  def __exit__(self, _exc_type, _exc_val, _exc_tb):
+    """Exit the context and remove the lock file."""
+    if os.path.exists(self.lockfile):
+      os.remove(self.lockfile)
